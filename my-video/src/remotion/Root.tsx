7cf25819f33f85fade876dc49e85b2d5eeedc1fc
Özal Elçioğlu <ozalelcioglu@Mir-MacBook-Air.local>
2026-03-09 23:19:34 +0100

    fix remotion root default import

--- my-video/src/remotion/Root.tsx
+++ my-video/src/remotion/Root.tsx
@@ -1,61 +1,28 @@
 import { Composition } from "remotion";
 import { Main } from "./MyComp/Main";
 import { NextLogo } from "./MyComp/NextLogo";
-import {
-  COMP_NAME,
-  defaultMyCompProps,
-  VIDEO_FPS,
-  getCompositionDurationInFrames,
-  getVideoDimensions,
-  type MyCompProps,
-} from "./types/constants";
 
-export const RemotionRoot: React.FC = () => {
+export default function RemotionRoot() {
   return (
     <>
-      <Composition<MyCompProps>
-        id={COMP_NAME}
+      <Composition
+        id="Main"
         component={Main}
-        fps={VIDEO_FPS}
-        defaultProps={defaultMyCompProps}
         durationInFrames={200}
+        fps={30}
         width={1280}
         height={720}
-        calculateMetadata={({ props }) => {
-          const mergedProps: MyCompProps = {
-            ...defaultMyCompProps,
-            ...props,
-            storyboard: props.storyboard ?? defaultMyCompProps.storyboard,
-          };
-
-          const ratio =
-            mergedProps.ratio ??
-            mergedProps.storyboard?.ratio ??
-            "horizontal";
-
-          const { width, height } = getVideoDimensions(ratio);
-          const durationInFrames =
-            getCompositionDurationInFrames(mergedProps);
-
-          return {
-            props: mergedProps,
-            durationInFrames,
-            width,
-            height,
-            fps: VIDEO_FPS,
-          };
-        }}
       />
 
       <Composition
         id="NextLogo"
         component={NextLogo}
-        durationInFrames={300}
+        durationInFrames={120}
         fps={30}
-        width={140}
-        height={140}
+        width={1280}
+        height={720}
         defaultProps={{ outProgress: 0 }}
       />
     </>
   );
-};
+}
--- my-video/src/remotion/index.ts
+++ my-video/src/remotion/index.ts
@@ -1,5 +1,5 @@
 import { registerRoot } from "remotion";
 import "../../styles/global.css";
-import { RemotionRoot } from "./Root";
+import RemotionRoot from "./Root";
 
 registerRoot(RemotionRoot);
