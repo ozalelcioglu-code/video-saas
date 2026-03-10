f73173720f1c9e10792eefd3453a50b2fe4f4aa7
Özal Elçioğlu <ozalelcioglu@Mir-MacBook-Air.local>
2026-03-10 18:42:59 +0100

    disable turbopack for vercel build

--- my-video/next.config.js
+++ my-video/next.config.js
@@ -1,19 +1,12 @@
-const BUILD_DIR = require("./build-dir.mjs");
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {
+    enabled: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  outputFileTracingIncludes: {
-    "/api/render": [
-      "./" + BUILD_DIR + "/**/*",
-      "./render.ts",
-      "./ensure-browser.ts",
-    ],
   },
 };
 
