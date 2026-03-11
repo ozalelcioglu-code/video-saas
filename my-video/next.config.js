8c801bc486c085d09e8ee701d292b39cbca7ce3c
Özal Elçioğlu <ozalelcioglu@Mir-MacBook-Air.local>
2026-03-11 22:01:35 +0100

    fix remotion bundle tracing

--- my-video/next.config.js
+++ my-video/next.config.js
@@ -1,12 +1,21 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
     enabled: false,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/render": ["./remotion-bundle/**/*"],
+    },
   },
 };
 
